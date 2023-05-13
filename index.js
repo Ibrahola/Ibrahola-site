6bb1339db36e156ebededa9b66345b208388a6a9
Ibraheem Bakare <bakare.ibraheem.ola@gmail.com>
2023-05-13 14:14:47 -0700

    commit changes

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+let date = document.querySelector(".date")
+let copy = document.querySelector(".copy")
+
+
+let myDate = new Date()
+
+date.innerHTML = myDate.getFullYear()
+
+copy.style.color = "white"
